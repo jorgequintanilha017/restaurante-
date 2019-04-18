fb6457d498947e871f9f7c154b6191aef5584eb8
jorgequintanilha017 <jorgeqs11@gmail.com>
2019-04-18 18:15:06 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,59 +1,52 @@
-let staticCacheName = 'restaurant-reviews-static-v1';
-const cacheUrls = [
-  './',
-  './favicon.png',
-  './manifest.json',
-  './index.html',
-  './restaurant.html',
-  './css/styles.css',
-  './data/restaurants.json',
-  './js/dbhelper.js',
-  './js/main.js',
-  './js/restaurant_info.js',
-  './img/1.jpg',
-  './img/2.jpg',
-  './img/3.jpg',
-  './img/4.jpg',
-  './img/5.jpg',
-  './img/6.jpg',
-  './img/7.jpg',
-  './img/8.jpg',
-  './img/9.jpg',
-  './img/10.jpg'
-];
+var staticCacheName = 'restaurants-v1';
 
-if (navigator.serviceWorker) {
-  window.addEventListener('load', _ => {
-    navigator.serviceWorker.register('./sw.js')
-      .then(_ => console.log('Service worker registered.'))
-      .catch(_ => console.log('Failed to register Service Worker.'))
-  });
-}
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(staticCacheName)
-      .then(cache => cache.addAll(cacheUrls))
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'js/dbhelper.js',
+        'js/personalScript.js',
+        'js/restaurant_info.js',
+        'css/styles.css',
+        'data/restaurants.json',
+        '/index.html',
+        '/restaurant.html',
+        'img/1.jpg',        
+        'img/2.jpg',        
+        'img/3.jpg',        
+        'img/4.jpg',        
+        'img/5.jpg',        
+        'img/6.jpg',        
+        'img/7.jpg',        
+        'img/8.jpg',        
+        'img/9.jpg',        
+        'img/10.jpg',        
+      ]);
+    })
   );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
-        return cacheNames.filter(name => {
-          return name.startsWith('restaurant-') && name != staticCacheName
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurants-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
         })
-      })
-      .then(cacheNames => Promise.all(cacheNames))
-      .then(cacheNames => cacheNames.map(name => caches.delete(name)))
-      .catch(error => console.log(error))
+      );
+    })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(response => response || fetch(e.request))
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
